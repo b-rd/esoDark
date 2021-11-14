edfbcd4627078704b8dd5ead3d049e2513291cf6
berd <88917909+b-rd@users.noreply.github.com>
2021-11-13 18:52:09 -0800

    Initial upload

---
+++ skin.php
@@ -0,0 +1,40 @@
+<?php
+// Plastic/skin.php
+// Plastic skin file.
+
+if (!defined("IN_ESO")) exit;
+
+class Zandronum extends Skin {
+
+var $name = "Zandronum";
+var $version = "0.1.0";
+var $author = "berd";
+var $numberOfColors = 27;
+
+// Add stylesheets and a favicon to the page header.
+function init()
+{
+	global $config;
+	$this->eso->addCSS("skins/base.css");
+	$this->eso->addCSS("skins/{$config["skin"]}/styles.css");
+	$this->eso->addCSS("skins/ie6.css", "ie6");
+	$this->eso->addCSS("skins/ie7.css", "ie7");
+	$this->eso->addToHead("<link rel='shortcut icon' type='image/ico' href='skins/{$config["skin"]}/favicon.ico'/>");
+}
+
+// Generate button HTML.
+function button($attributes)
+{
+	$class = $id = $style = ""; $attr = " type='submit'";
+	foreach ($attributes as $k => $v) {
+		if ($k == "class") $class = " $v";
+		elseif ($k == "id") $id = " id='$v'";
+		elseif ($k == "style") $style = " style='$v'";
+		else $attr .= " $k='$v'";
+	}
+	return "<span class='button$class'$id$style><input$attr/></span>";
+}
+
+}
+
+?>
